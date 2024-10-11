9b8b88ec79939d96f6b632a49798df5c71cc0e6f
Marcin Żuk <Gnoneks@gmail.com>
2024-10-11 10:56:22 +0200

    002--Implement adding note functionality

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const addFirstNoteButton = document.getElementById("no-notes-button");
+const addNewNoteForm = document.getElementById("add-new-note-form");
+const addNewNoteCancelButton = document.getElementById("add-new-note-cancel");
 
-addNote = () => {
-  console.log("Adding note");
+toggleAddNoteMenu = () => {
+  const isMenuVisible = addNewNoteForm.style.display === "flex";
+
+  addNewNoteForm.style.display = isMenuVisible ? "none" : "flex";
 };
 
-addFirstNoteButton.addEventListener("click", addNote);
+addNote = (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const formValues = Object.fromEntries(formData);
+  formValues.id = Math.floor(Math.random() * 1000000000);
+
+  toggleAddNoteMenu();
+};
+
+addFirstNoteButton.addEventListener("click", toggleAddNoteMenu);
+addNewNoteCancelButton.addEventListener("click", toggleAddNoteMenu);
+addNewNoteForm.addEventListener("submit", addNote);
