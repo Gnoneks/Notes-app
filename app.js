d045b6e80087a3920b291a97850913d27df85ab7
Marcin Żuk <Gnoneks@gmail.com>
2024-10-11 15:17:37 +0200

    003--Add note editing Adjust some class names

--- app.js
+++ app.js
@@ -1,54 +1,97 @@
 const addFirstNoteButton = document.getElementById("no-notes-button");
-const addNewNoteForm = document.getElementById("add-new-note-form");
-const addNewNoteCancelButton = document.getElementById("add-new-note-cancel");
+const editNoteForm = document.getElementById("edit-note-form");
+const editNoteTitle = document.getElementById("edit-note-title");
+const editNoteDetails = document.getElementById("edit-note-details");
+const editNoteCancel = document.getElementById("edit-note-cancel");
 const notes = document.getElementById("notes");
 
-toggleAddNoteMenu = () => {
-  const isMenuVisible = addNewNoteForm.style.display === "flex";
+const notesData = [];
+let editedNote = null;
 
-  addNewNoteForm.style.display = isMenuVisible ? "none" : "flex";
+const toggleNoteMenu = (_, note) => {
+  const isOpeningMenu = editNoteForm.style.display === "none";
+
+  if (isOpeningMenu) {
+    if (note) {
+      editedNote = note;
+      updateNoteForm(note);
+    }
+  } else {
+    editedNote = null;
+    updateNoteForm({ title: "", details: "" });
+  }
+
+  editNoteForm.style.display = isOpeningMenu ? "flex" : "none";
 };
 
-addNote = (event) => {
-  event.preventDefault();
+const updateNotesView = () => {
+  notes.innerHTML = "";
 
-  const formData = new FormData(event.target);
-  const formValues = Object.fromEntries(formData);
-  formValues.date = new Date();
-  formValues.id = Math.floor(Math.random() * 1000000000);
-  console.log("Note: ", formValues);
+  notesData.forEach((note) => {
+    const noteListElement = document.createElement("li");
+    noteListElement.className = "note";
 
-  createNoteElement(formValues);
-  toggleAddNoteMenu();
-};
+    const month = note.date.toLocaleString("default", { month: "long" });
+    const day = note.date.getDate();
 
-const createNoteElement = ({ title, details, date }) => {
-  const noteListElement = document.createElement("li");
-  noteListElement.className = "notes-note";
-
-  const month = date.toLocaleString("default", { month: "long" });
-  const day = date.getDate();
-
-  const noteContent = `
-        <div class="notes-note-header">
-            <span class="notes-note-title">${title}</span>
-            <div class="notes-note-buttons">
-                <button class="notes-note-button">
+    const noteContent = `
+        <div class="note-header">
+            <p class="note-title">${note.title}</p>
+            <div class="note-buttons">
+                <button class="note-button" name="edit-note">
                     <img src="icons/edit.svg" alt="Edit note - Three lines with pencil" width="20" height="20" />
                 </button>
-                <button class="notes-note-button">
+                <button class="note-button" name="remove-note">
                     <img src="icons/trash-bin.svg" alt="Remove note - Trash bin" width="20" height="20" />
                 </button>
             </div>
         </div>
-        <span class="notes-note-details">${details}</span>
-        <span class="notes-note-date">${month} ${day}</span>
+        <p class="note-details">${note.details}</p>
+        <p class="note-date">${month} ${day}</p>
         `;
 
-  noteListElement.innerHTML = noteContent;
-  notes.appendChild(noteListElement);
+    noteListElement.innerHTML = noteContent;
+
+    const editButton = noteListElement.querySelector("[name='edit-note']");
+    editButton.addEventListener("click", function () {
+      toggleNoteMenu(null, note);
+    });
+
+    const removeButton = noteListElement.querySelector("[name='edit-note']");
+
+    notes.appendChild(noteListElement);
+  });
 };
 
-addFirstNoteButton.addEventListener("click", toggleAddNoteMenu);
-addNewNoteCancelButton.addEventListener("click", toggleAddNoteMenu);
-addNewNoteForm.addEventListener("submit", addNote);
+const updateNoteForm = (note) => {
+  editNoteTitle.value = note.title;
+  editNoteDetails.value = note.details;
+};
+
+const updateNote = (event) => {
+  event.preventDefault();
+
+  const formValues = Object.fromEntries(new FormData(event.target));
+  formValues.date = new Date();
+
+  if (editedNote) {
+    const editedNoteIdx = notesData.findIndex(
+      ({ id }) => id.toString() === editedNote.id.toString()
+    );
+    formValues.id = editedNote.id;
+
+    notesData[editedNoteIdx] = formValues;
+    editedNote = null;
+  } else {
+    formValues.id = Math.floor(Math.random() * 1000000000);
+
+    notesData.push(formValues);
+  }
+
+  updateNotesView();
+  toggleNoteMenu();
+};
+
+addFirstNoteButton.addEventListener("click", toggleNoteMenu);
+editNoteCancel.addEventListener("click", toggleNoteMenu);
+editNoteForm.addEventListener("submit", updateNote);
